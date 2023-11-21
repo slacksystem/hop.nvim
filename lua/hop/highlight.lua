71dce037eea009cf47161419e831ade88cd70510
slack <slack@slacksystem.com>
2023-11-21 14:42:06 -0500

    style: Update color codes for HopNextKey highlights
    - Adjust key highlight color codes in 'highlight.lua' file for better code readability.
    - Enhance user interface aesthetics by modifying `HopNextKey` and `HopNextKey2` highlight colors.

--- lua/hop/highlight.lua
+++ lua/hop/highlight.lua
@@ -6,13 +6,13 @@ function M.insert_highlights()
   local set = vim.api.nvim_set_hl
 
   -- Highlight used for the mono-sequence keys (i.e. sequence of 1).
-  set(0, 'HopNextKey', { fg = '#ff007c', bold = true, ctermfg = 198, cterm = { bold = true }, default = true })
+  set(0, 'HopNextKey', { fg = '#ffee7c', bold = true, ctermfg = 198, cterm = { bold = true }, default = true })
 
   -- Highlight used for the first key in a sequence.
   set(0, 'HopNextKey1', { fg = '#00dfff', bold = true, ctermfg = 45, cterm = { bold = true }, default = true })
 
   -- Highlight used for the second and remaining keys in a sequence.
-  set(0, 'HopNextKey2', { fg = '#2b8db3', ctermfg = 33, default = true })
+  set(0, 'HopNextKey2', { fg = '#0096ff', ctermfg = 33, default = true })
 
   -- Highlight used for the unmatched part of the buffer.
   set(0, 'HopUnmatched', { fg = '#666666', sp = '#666666', ctermfg = 242, default = true })
